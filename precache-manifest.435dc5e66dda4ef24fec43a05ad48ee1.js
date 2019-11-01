89e236479629e64f17c456bb3d6394a8e190595a
Sveta K <test@test.test>
2019-11-01 14:56:02 +0000

    Updates

---
+++ precache-manifest.435dc5e66dda4ef24fec43a05ad48ee1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5096de5c8c1262e4a1f67871180e22d4",
+    "url": "/cv-website/index.html"
+  },
+  {
+    "revision": "5dd21a286aeeba93e5f1",
+    "url": "/cv-website/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2dc43a28e1bb36b63d9c",
+    "url": "/cv-website/static/css/main.30af7ae2.chunk.css"
+  },
+  {
+    "revision": "5dd21a286aeeba93e5f1",
+    "url": "/cv-website/static/js/2.7cda739a.chunk.js"
+  },
+  {
+    "revision": "2dc43a28e1bb36b63d9c",
+    "url": "/cv-website/static/js/main.a146cba6.chunk.js"
+  },
+  {
+    "revision": "31085d5af1e022ea345c",
+    "url": "/cv-website/static/js/runtime-main.753390fe.js"
+  },
+  {
+    "revision": "78986ed709edf9d1320e8cd8ea8d3856",
+    "url": "/cv-website/static/media/HeroImage.78986ed7.jpg"
+  },
+  {
+    "revision": "a5c5a45dea66a88051c587affaf24631",
+    "url": "/cv-website/static/media/HeroImage_5.a5c5a45d.jpg"
+  },
+  {
+    "revision": "09d57372cfc353b66ee24c9f262b9358",
+    "url": "/cv-website/static/media/star49_1.09d57372.jpg"
+  }
+]);
