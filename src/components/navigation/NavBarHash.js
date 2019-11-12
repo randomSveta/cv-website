f281dc380e70804cea57efd565c743b391312030
Sveta K <test@test.test>
2019-11-12 00:18:07 +0000

    Fix NavBarHash for the Blog page - the case when the page doesn't need NavBarHash (Sections navigation)

--- src/components/navigation/NavBarHash.js
+++ src/components/navigation/NavBarHash.js
@@ -10,31 +10,39 @@ import { faHashtag } from '@fortawesome/free-solid-svg-icons'
 
 export default function NavBarHash(props) {
     const hashLinksList = props.links;
-    const hashLinks = hashLinksList.map(link => {
+
+    let hashLinks = [];
+    if (hashLinksList) {
+        hashLinks = hashLinksList.map(link => {
+            return (
+                <NavItem key={link.id} className="navbar-li-hash">
+                    <NavHashLink smooth /*activeClassName= 'active-hash-item'*/ className='link-style-hash m-1 p-1 text-decoration-none' to={link.hashUrl}
+                    >{link.name}</NavHashLink>
+                </NavItem>
+            );
+        });
+    }
+
+    if (hashLinks.length !== 0) {
         return (
-            <NavItem key={link.id} className="navbar-li-hash">
-                <NavHashLink smooth /*activeClassName= 'active-hash-item'*/ className='link-style-hash m-1 p-1 text-decoration-none' to={link.hashUrl}
-                >{link.name}</NavHashLink>
-            </NavItem>
+            <div className="d-flex flex-md-row-reverse flex-column justify-content-center align-items-center">
+
+                <Button color="secondary" id="hash-list-toggler"><FontAwesomeIcon icon={faHashtag} className="active-item" /></Button>
+                <UncontrolledTooltip placement="left" target="hash-list-toggler">
+                    Sections navigation
+                </UncontrolledTooltip>
+                <UncontrolledCollapse toggler="#hash-list-toggler">
+                    <Nav>
+                        {hashLinks}
+
+                    </Nav>
+                </UncontrolledCollapse>
+            </div>
+
         );
-    });
-
-
-    return (
-        <div className="d-flex flex-md-row-reverse flex-column justify-content-center align-items-center">
-
-            <Button color="secondary" id="hash-list-toggler"><FontAwesomeIcon icon={faHashtag} className="active-item" /></Button>
-            <UncontrolledTooltip placement="left" target="hash-list-toggler">
-                Sections navigation
-            </UncontrolledTooltip>
-            <UncontrolledCollapse toggler="#hash-list-toggler">
-                <Nav>
-                    {hashLinks}
-
-                </Nav>
-            </UncontrolledCollapse>
-        </div>
-
-    );
+    }
+    else {
+        return null;
+    }
 }
 
