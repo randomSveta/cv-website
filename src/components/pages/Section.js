fefed61bf437e06c06f31a27d08a7c1b413faaeb
Sveta K <test@test.test>
2019-11-17 00:06:23 +0000

    Add a column size 8 for xs screens

--- src/components/pages/Section.js
+++ src/components/pages/Section.js
@@ -6,8 +6,8 @@ export default function Section(props) {
     return (
         <section id={props.sectionId} className="m-0 p-0 container mw-100 bg-light">
             <Row className="m-0 p-0  h2-row justify-content-md-start align-items-center justify-content-center align-items-center">
-                <Col xs="3" className="m-0 p-0">
-                    <h2 className="p-3 m-0 text-white d-flex flex-row justify-content-center align-items-center">{props.name}
+                <Col md="3" xs="8" className="m-0 p-0">
+                    <h2 className="p-2 m-0  text-white d-flex text-md-left text-center  flex-row justify-content-center align-items-center">{props.name}
                     </h2>
                 </Col>
             </Row>
