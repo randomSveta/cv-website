529606c686878037a68ca840f4aec1bd6bd4c5eb
Sveta K <test@test.test>
2019-11-18 14:57:04 +0000

    Change the footer article title

--- src/components/pages/blog/articles.js
+++ src/components/pages/blog/articles.js
@@ -8,13 +8,13 @@ export const ARTICLES = [
     {
         id: 1,
         url: '/place-footer-at-the-bottom/',
-        title: 'Place Footer at the bottom',
+        title: 'Keep footer at the bottom',
         shortDescription: "If a page too short and footer is placed at the middle of the page let's force fotter to take it place and stay at the bootom no matter what.",
-        jsx: <BottomFooterArticle />,
+        jsx: <BottomFooterArticle/>,
         images: [{
             id: 1,
             path: require('../../../assets/images/blog-page/articles/footer-at-the-bottom.png') ,
-            alt: 'Footer at the bottom'
+            alt: 'Keep footer at the bottom'
         },
         { id:2,
             path: require('../../../assets/images/blog-page/articles/let-const-var-title.png'),
