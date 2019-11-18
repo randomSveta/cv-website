2a72cd71a55eaea777a30b25545eeec91d3e5bb1
Sveta K <test@test.test>
2019-11-18 14:55:15 +0000

    Add layout for articles

--- src/components/pages/blog/articles-text-components/BottomFooterArticle.js
+++ src/components/pages/blog/articles-text-components/BottomFooterArticle.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Row, Col} from 'reactstrap';
 
 export default function BottomFooterArticle(props) {
+
     return (
-            <Row>
-                <Col xs='12'>
-          <div>FFFFFF</div>
-                </Col>
-            </Row>
+        <React.Fragment>
+            <section className="">
+                <h2 className="">Position Absolute</h2>
+            </section>
+            <section className="">
+                <h2>Fixed</h2>
+            </section>
+            <section className="">
+                <h2>Padding</h2>
+            </section>
+        </React.Fragment>
+
     );
 }
--- src/components/pages/blog/articles-text-components/LetsConstVarArticle.js
+++ src/components/pages/blog/articles-text-components/LetsConstVarArticle.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
 
 export default function LetConstVarArticle(props) {
+
     return (
-        <Row>
-            <Col xs='12'>
-                <div>FFFFFF</div>
-            </Col>
-        </Row>
+        <React.Fragment>
+            <section className="">
+                <h2 className="">Var</h2>
+            </section>
+            <section className="">
+                <h2>Const</h2>
+            </section>
+            <section className="">
+                <h2>let</h2>
+            </section>
+        </React.Fragment>
+
     );
 }
