d5c86106d0d4d2da08e5be4583b70440aa191b97
Sveta K <test@test.test>
2019-11-18 11:39:05 +0000

    Add layout and elements to the GitHub projects section.

--- src/components/pages/portfolio/sections/github-projects/GitHubProject.js
+++ src/components/pages/portfolio/sections/github-projects/GitHubProject.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { Card, CardLink, CardTitle, CardText } from 'reactstrap';
+import {
+    Card,
+    CardLink,
+    CardText,
+    CardFooter,
+    CardBody,
+    CardHeader
+} from 'reactstrap';
 
 export default function GitHubProject(props) {
 
     return (
-        <Card body>
-            <CardTitle>{props.project.name}</CardTitle>
-            <CardText>{props.project.description}</CardText>
-            <CardLink href={props.project.linkRepo} target="_blank">Repository</CardLink>
-            <CardLink href={props.project.linkPages} target="_blank">Pages</CardLink>
+        <Card className="gh-project rounded-0">
+            <CardHeader className="text-center gh-card-bg rounded-0 text-white">{props.project.name}</CardHeader>
+            <CardBody>
+                <CardText>{props.project.description}</CardText>
+            </CardBody>
+            <CardFooter className="text-center gh-card-bg rounded-0">
+                {props.project.taskLink ?
+                    <CardLink href={props.project.taskLink} target="_blank" className="link-style underscore">Task</CardLink>
+                    : null
+                }
+                {props.project.repoLink ?
+                    <CardLink href={props.project.repoLink} target="_blank" className="link-style underscore">Repository</CardLink>
+                    : null
+                }
+                {props.project.projectLink ?
+                    <CardLink href={props.project.projectLink} target="_blank" className="link-style underscore">Pages</CardLink>
+                    : null}
+            </CardFooter>
         </Card>
     );
 }
