e74ce2e8d4c9520de06634575af3807031baa05b
Sveta K <test@test.test>
2019-11-18 11:43:46 +0000

    Add a new link and element for link. Change margin. Add new class

--- src/components/pages/portfolio/sections/codepen-projects/CodePenProject.js
+++ src/components/pages/portfolio/sections/codepen-projects/CodePenProject.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExpand } from '@fortawesome/free-solid-svg-icons';
+import { faExpand, faThumbtack } from '@fortawesome/free-solid-svg-icons';
 
 export default function CodePenProject(props) {
     return (
-        <div className="m-3 p-3">
-            {props.project}
-            <a href={props.projectLink} target="_blank" rel="noopener noreferrer" className="border"><FontAwesomeIcon icon={faExpand} /> full page</a>
+        <div className="m-1 p-1">
+            {props.project.jsx}
+            <div className="bg-info p-2">
+                <div className="cp-projects-links-bg p-2">
+                    {props.project.taskLink ?
+                        <a href={props.project.taskLink} target="_blank" rel="noopener noreferrer" className="link-style m-2 underscore text-decoration-none"><FontAwesomeIcon icon={faThumbtack} />  Task</a>
+                        : null
+                    }
+                    <a href={props.project.projectLink} target="_blank" rel="noopener noreferrer" className="link-style m-2 underscore text-decoration-none"><FontAwesomeIcon icon={faExpand} />  Full page</a>
+                </div>
+
+            </div>
         </div>
     );
 }
