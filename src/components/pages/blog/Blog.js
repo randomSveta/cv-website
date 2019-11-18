b25c7cc04a70601360a59d893645aa1a16e2ba0c
Sveta K <test@test.test>
2019-11-18 14:11:25 +0000

    Add layout, elements, styles to the Blog page

--- src/components/pages/blog/Blog.js
+++ src/components/pages/blog/Blog.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import ArticlesDisplay from './ArticlesDisplay'
+import { Container, Row, Col } from 'reactstrap';
+import ArticlesDisplay from './ArticlesDisplay';
+import Section from '../Section';
 
 export default function Blog(props) {
     return (
-        <React.Fragment>
-            <ArticlesDisplay sectionUrl='/blog'/>
-        </React.Fragment>
- 
+        <Container id="blog">
+            <Row className="m-0 p-0 justify-content-center align-items-center m-0 p-0 ">
+                <Col xs='12' className="m-0 p-0 ">
+                    <h1 className="text-white">Blog</h1>
+                </Col>
+            </Row>
+            <Row className="m-0 p-0 justify-content-center align-items-center m-0 p-0">
+                <Col xs='12' className="m-0 p-0">
+                    <Section content={<ArticlesDisplay sectionUrl='/blog' />} name="Articles" sectionId="articles" />
+                </Col>
+            </Row>
+        </Container>
+
+
     );
 }
