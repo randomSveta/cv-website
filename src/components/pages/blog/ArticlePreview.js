e430f6084b6dd99ce3d8c8f32a0a1ff951a20e24
Sveta K <test@test.test>
2019-11-18 14:09:02 +0000

    Add a  card footer to the ArticlePreview

--- src/components/pages/blog/ArticlePreview.js
+++ src/components/pages/blog/ArticlePreview.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle
-  } from 'reactstrap';
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardFooter
+} from 'reactstrap';
+
 
 export default function ArticlePreview(props) {
 
-const linkPath = props.sectionUrl.concat(props.article.url);
+  const linkPath = props.sectionUrl.concat(props.article.url);
+  const titleImagePath = props.article.images[0].path;
 
-console.log(linkPath);
-    return (
-        <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>{props.article.title}</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>{props.article.shortDescription}</CardText>
-          <Link to={linkPath}>Link</Link>
-        </CardBody>
-      </Card>
-    );
+  return (
+    <Card className="rounded-0 article-preview-card">
+      <CardImg top width="100%" src={titleImagePath} alt="Card image cap" />
+      <CardBody>
+        <CardTitle className="font-weight-bold text-center">{props.article.title}</CardTitle>
+        <CardText>{props.article.shortDescription}</CardText>
+      </CardBody>
+      <CardFooter className="rounded-0 text-center art-card-bg"><Link to={linkPath} className="link-style underscore text-decoration-none">Full article</Link></CardFooter>
+    </Card>
+  );
 }
