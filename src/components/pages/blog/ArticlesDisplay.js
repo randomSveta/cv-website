15c679a7f57449f173f62c2c99c10c5702e7c746
Sveta K <test@test.test>
2019-11-18 14:09:36 +0000

    Add key

--- src/components/pages/blog/ArticlesDisplay.js
+++ src/components/pages/blog/ArticlesDisplay.js
@@ -3,18 +3,18 @@ import { ARTICLES } from './articles';
 import { Row, CardColumns, Container } from 'reactstrap';
 import ArticlePreview from './ArticlePreview';
 
-export default function GitHubProjectsDisplay(props) {
+export default function ArticlesDisplay(props) {
 
     const articles = ARTICLES.sort((a, b) => b.year - a.year).map(article => {
         return (
-            <ArticlePreview article={article} sectionUrl={props.sectionUrl}/>
+            <ArticlePreview article={article} sectionUrl={props.sectionUrl} key={article.id} />
         );
     });
 
     return (
         <Container>
             <Row className="mx-5 mt-1 mb-5 px-5 justify-content-center align-items-center">
-                <CardColumns className="m-3 p-3"> 
+                <CardColumns className="m-3 p-3">
                     {articles}
                 </CardColumns>
             </Row>
