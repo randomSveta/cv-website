9471852f89b95a6774d5320234bbc37643cc9cf5
Sveta K <test@test.test>
2019-11-18 14:56:09 +0000

    Remove one row. Comment an added img element

--- src/components/pages/blog/ArticlePage.js
+++ src/components/pages/blog/ArticlePage.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
 export default function ArticlePage(props) {
+    //const topImgTop = props.article.images[0].path;
+    //const altImgTop = props.article.images[0].alt;
+    
     return (
-        <Container>
+        <Container className="bg-white">
             <Row>
                 <Col xs='12'>
+                    {/*<img className="w-100" src={topImgTop} alt={altImgTop} />*/ }
                     <h1>{props.article.title}</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs='12'>
-                    <div/>
                 </Col>
             </Row>
             <Row>
