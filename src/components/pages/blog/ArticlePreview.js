53ebc4425eb8bc0b3815da595c3273f46ff6f72a
Sveta K <test@test.test>
2019-11-25 12:19:58 +0000

    Change the look of the article preview card

--- src/components/pages/blog/ArticlePreview.js
+++ src/components/pages/blog/ArticlePreview.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardFooter
+  Card, CardImg, CardText, CardBody, CardHeader, CardFooter
 } from 'reactstrap';
 
 
@@ -13,12 +12,12 @@ export default function ArticlePreview(props) {
 
   return (
     <Card className="rounded-0 article-preview-card">
+      <CardHeader className="font-weight-bold text-center rounded-0 art-card-bg"><Link to={linkPath} className="link-style underscore text-decoration-none">{props.article.title}</Link></CardHeader>
       <CardImg top width="100%" src={titleImagePath} alt="Card image cap" />
       <CardBody>
-        <CardTitle className="font-weight-bold text-center">{props.article.title}</CardTitle>
         <CardText>{props.article.shortDescription}</CardText>
       </CardBody>
-      <CardFooter className="rounded-0 text-center art-card-bg"><Link to={linkPath} className="link-style underscore text-decoration-none">Full article</Link></CardFooter>
+      <CardFooter className="rounded-0 text-center art-card-bg"><Link to={linkPath} className="link-style underscore text-decoration-none">Full article >></Link></CardFooter>
     </Card>
   );
 }
