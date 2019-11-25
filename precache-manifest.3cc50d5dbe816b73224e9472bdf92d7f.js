60c8fbfb9052b58d3cfb53d82bf47bf1294f78a0
Sveta K <test@test.test>
2019-11-25 12:25:40 +0000

    Updates

---
+++ precache-manifest.3cc50d5dbe816b73224e9472bdf92d7f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd44cbc95aaab58dd941477ce9abefe3",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "d3abe2b03c577eefce91",
+    "url": "/my-website/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1a17dd65791ba563c993",
+    "url": "/my-website/static/css/main.0e123c7c.chunk.css"
+  },
+  {
+    "revision": "d3abe2b03c577eefce91",
+    "url": "/my-website/static/js/2.7149aa8f.chunk.js"
+  },
+  {
+    "revision": "1a17dd65791ba563c993",
+    "url": "/my-website/static/js/main.3b63301f.chunk.js"
+  },
+  {
+    "revision": "7b64d4b972495e336675",
+    "url": "/my-website/static/js/runtime-main.6b26dad8.js"
+  },
+  {
+    "revision": "8354991642d624fcd84e8825c503abb6",
+    "url": "/my-website/static/media/about_parallax_bw.83549916.jpg"
+  },
+  {
+    "revision": "b37ac1362843d987fd060984b1148567",
+    "url": "/my-website/static/media/blog-parallax-bw.b37ac136.jpg"
+  },
+  {
+    "revision": "8d91a0a3e59a671430834db7be1ea849",
+    "url": "/my-website/static/media/current_p_parallax_bw.8d91a0a3.jpg"
+  },
+  {
+    "revision": "e180d7cefee3f91a461963b58649d345",
+    "url": "/my-website/static/media/footer-start-html.e180d7ce.png"
+  },
+  {
+    "revision": "a5114bb87e5630dce43e664183898f80",
+    "url": "/my-website/static/media/gh-projects-parallax-bw.a5114bb8.jpg"
+  },
+  {
+    "revision": "f0fb149619f10e6fffa851c8dbd3c82f",
+    "url": "/my-website/static/media/hero_image.f0fb1496.jpg"
+  },
+  {
+    "revision": "71142a7ca06cef472ed5d55ed9947394",
+    "url": "/my-website/static/media/let-const-var-preview-img.71142a7c.png"
+  },
+  {
+    "revision": "9644baa7a4fa422a927b7ded47cfe982",
+    "url": "/my-website/static/media/min-height-100-200px.9644baa7.png"
+  },
+  {
+    "revision": "1df746cea5c6ac388cb5da61c47d4adc",
+    "url": "/my-website/static/media/min-height-100.1df746ce.png"
+  },
+  {
+    "revision": "0b1478c3a7bf5383d5e760f9b61e1026",
+    "url": "/my-website/static/media/pos-absolute-1.0b1478c3.png"
+  },
+  {
+    "revision": "9fa07baff5054aea186f38a6aa6f81eb",
+    "url": "/my-website/static/media/pos-absolute-2.9fa07baf.png"
+  },
+  {
+    "revision": "e7c32c2e81d8387a98cb7f89b82180f0",
+    "url": "/my-website/static/media/pos-absolute-3.e7c32c2e.png"
+  },
+  {
+    "revision": "7f20bbc1d9fe593a0e962d19a92223e9",
+    "url": "/my-website/static/media/preview-image-1.7f20bbc1.png"
+  },
+  {
+    "revision": "e452a8b22c189ee1e23bd96882bfa735",
+    "url": "/my-website/static/media/preview-image-2.e452a8b2.png"
+  },
+  {
+    "revision": "7e267f3c644fb92b07c9543a3654abdb",
+    "url": "/my-website/static/media/preview-image-3.7e267f3c.png"
+  },
+  {
+    "revision": "6f6ad9a2428ee7cd0f4f921e9ac1051a",
+    "url": "/my-website/static/media/skills_parallax_bw.6f6ad9a2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-website/precache-manifest.45873c44f39daab53f75fb9ab4d56bd9.js"
+  "/my-website/precache-manifest.3cc50d5dbe816b73224e9472bdf92d7f.js"
 );
 
 self.addEventListener('message', (event) => {
