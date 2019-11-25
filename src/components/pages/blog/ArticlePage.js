769f357ebf53324613ab4ed55e5157e3ac3666f2
Sveta K <test@test.test>
2019-11-25 12:09:43 +0000

    Change layout  and add links to the Blog

--- src/components/pages/blog/ArticlePage.js
+++ src/components/pages/blog/ArticlePage.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 export default function ArticlePage(props) {
     //const topImgTop = props.article.images[0].path;
     //const altImgTop = props.article.images[0].alt;
-    
+
     return (
-        <Container className="bg-white">
+        <Container className="">
             <Row>
+                <Col className="text-center p-3">
+                    <Link className="link-style underscore text-decoration-none" to={'/blog'}><FontAwesomeIcon icon={faArrowLeft} /> Blog</Link>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs="2">
+                </Col>
                 <Col xs='12'>
-                    {/*<img className="w-100" src={topImgTop} alt={altImgTop} />*/ }
-                    <h1>{props.article.title}</h1>
+                    {/*<img className="w-100" src={topImgTop} alt={altImgTop} />*/}
+                    <h1 className="text-center text-white">{props.article.title}</h1>
                 </Col>
             </Row>
             <Row>
                 <Col xs='12'>
-                    {props.article.jsx}
+                    <article className="m-3 p-4 border bg-white">
+                        {props.article.jsx}
+                    </article>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="text-center p-3">
+                    <Link className="link-style underscore text-decoration-none" to={'/blog'}><FontAwesomeIcon icon={faArrowLeft} /> Blog</Link>
                 </Col>
             </Row>
         </Container>
