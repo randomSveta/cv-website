2395d0af8c87596a5aa446c4588bc02600bf006a
Sveta K <test@test.test>
2019-11-30 01:08:29 +0000

    Add a new component for the large image displaying

--- src/components/pages/blog/articles-text-components/ImageModal.js
+++ src/components/pages/blog/articles-text-components/ImageModal.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 export default function ImageModal(props) {
+
   return (
-      <Modal isOpen={props.modal} toggle={props.toggle} id={props.id} size="lg">
-<ModalHeader toggle={props.toggle}>{props.title}</ModalHeader>
-        <ModalBody>
-          {props.img}
-        </ModalBody>
-      </Modal>
+    <Modal isOpen={props.modal} toggle={props.toggle} size="lg">
+      <ModalHeader toggle={props.toggle}>{props.title}</ModalHeader>
+      <ModalBody>
+        <img width="100%" src={props.img} alt={props.title} />
+      </ModalBody>
+    </Modal>
   );
 }
-
