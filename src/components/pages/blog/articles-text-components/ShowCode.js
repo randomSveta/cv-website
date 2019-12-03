979dd3c8fd3ca713586d167ac0f5f8455616811b
Sveta K <test@test.test>
2019-12-03 15:10:56 +0000

    Change names of the file, link and component

--- src/components/pages/blog/articles-text-components/ShowCode.js
+++ src/components/pages/blog/articles-text-components/ShowCode.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
 
-export default function ShowCSS(props) {
+export default function ShowCode(props) {
+
+
     return (
         <div>
-            <Button color="info" id="final-css" style={{ marginBottom: '1rem', width: "100%" }}>
+            <Button color="info" id={props.codeId} style={{ marginBottom: '1rem', width: "100%" }}>
                 {props.button}
             </Button>
-            <UncontrolledCollapse toggler="#final-css">
+            <UncontrolledCollapse toggler={props.codeId}>
                 <Card>
                     <CardBody>
                         <code className="code-styles px-5 py-3 text-light bg-dark">
-                            {props.css}
+                            {props.code}
                         </code>
 
                     </CardBody>
