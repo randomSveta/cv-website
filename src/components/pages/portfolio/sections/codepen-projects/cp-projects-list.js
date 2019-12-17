bf3c45399339532efb12af7305ca2db163a84de6
Sveta K <test@test.test>
2019-12-17 22:34:27 +0000

    Change projects order. Add FCC Calculator project

--- src/components/pages/portfolio/sections/codepen-projects/cp-projects-list.js
+++ src/components/pages/portfolio/sections/codepen-projects/cp-projects-list.js
@@ -3,52 +3,55 @@ import React from 'react';
 export const CP_PROJECTS = [
   {
     id: 1,
-    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Drum Machine" src="https://codepen.io/randomsveta/embed/preview/PrLXLg?height=265&theme-id=0&default-tab=js,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
-      See the Pen <a href='https://codepen.io/randomsveta/pen/PrLXLg'>FCC: Drum Machine</a> by Sveta
+    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Personal Portfolio Webpage" src="https://codepen.io/randomsveta/embed/preview/odyppL?height=265&theme-id=0&default-tab=css,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+      See the Pen <a href='https://codepen.io/randomsveta/pen/odyppL'>FCC: Personal Portfolio Webpage</a> by Sveta
         (<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
       </iframe>,
-    name: 'FCC: Drum Machine',
-    taskLink: 'https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-drum-machine',
-    projectLink: 'https://codepen.io/randomsveta/full/PrLXLg',
-    year: 2019,
+    name: 'FCC: Personal Portfolio Webpage',
+    taskLink: 'https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-personal-portfolio-webpage',
+    projectLink: 'https://codepen.io/randomsveta/full/odyppL',
+    year: 2018,
     section: 'CodePen'
+
   },
   {
     id: 2,
-    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Markdown Previewer" src="https://codepen.io/randomsveta/embed/preview/xoXQgr?height=265&theme-id=0&default-tab=js,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
-      See the Pen <a href='https://codepen.io/randomsveta/pen/xoXQgr'>FCC: Markdown Previewer</a> by Sveta
+    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Adele-Tribute page" src="https://codepen.io/randomsveta/embed/preview/zjKRJb?height=265&theme-id=0&default-tab=html,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+      See the Pen <a href='https://codepen.io/randomsveta/pen/zjKRJb'>FCC: Adele-Tribute page</a> by Sveta
         (<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
       </iframe>,
-    name: 'FCC: Markdown Previewer',
-    taskLink: 'https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer',
-    projectLink: 'https://codepen.io/randomsveta/full/xoXQgr',
-    year: 2019,
+    name: 'FCC: Adele-Tribute page',
+    taskLink: 'https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-tribute-page',
+    projectLink: 'https://codepen.io/randomsveta/full/zjKRJb',
+    year: 2018,
     section: 'CodePen'
+
   },
   {
     id: 3,
-    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Random Quote Machine" src="https://codepen.io/randomsveta/embed/preview/oRRVVM?height=265&theme-id=0&default-tab=js,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
-      See the Pen <a href='https://codepen.io/randomsveta/pen/oRRVVM'>FCC: Random Quote Machine</a> by Sveta
+    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Product Landing Page" src="https://codepen.io/randomsveta/embed/preview/JZNWVZ?height=265&theme-id=default&default-tab=html,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+      See the Pen <a href='https://codepen.io/randomsveta/pen/JZNWVZ'>FCC: Product Landing Page</a> by Sveta
         (<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
       </iframe>,
-    name: 'FCC: Random Quote Machine',
-    taskLink: 'https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-random-quote-machine',
-    projectLink: 'https://codepen.io/randomsveta/full/oRRVVM',
+    name: 'FCC: Product Landing Page',
+    taskLink: 'https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-product-landing-page',
+    projectLink: 'https://codepen.io/randomsveta/full/JZNWVZ',
     year: 2019,
     section: 'CodePen'
+
   },
   {
     id: 4,
-    jsx: <iframe height="400" width='100%' scrolling="no" title="Avocado" src='https://codepen.io/randomsveta/embed/preview/drVOzX?height=265&theme-id=0&default-tab=css,result'
-      frameBorder="no" allowtransparency="true" allowFullScreen={true}>
-      See the Pen <a href='https://codepen.io/randomsveta/pen/drVOzX'>Avocado</a> by Sveta
-(<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
-</iframe>,
-    name: 'Avocado',
-    taskLink: '',
-    projectLink: 'https://codepen.io/randomsveta/full/drVOzX',
+    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Technical Documentation Page" src="https://codepen.io/randomsveta/embed/preview/QYQjBg?height=265&theme-id=0&default-tab=html,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+      See the Pen <a href='https://codepen.io/randomsveta/pen/QYQjBg'>FCC: Technical Documentation Page</a> by Sveta
+        (<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
+      </iframe>,
+    name: 'FCC: Technical Documentation Pages',
+    taskLink: 'https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-technical-documentation-page',
+    projectLink: 'https://codepen.io/randomsveta/full/QYQjBg',
     year: 2019,
     section: 'CodePen'
+
   },
   {
     id: 5,
@@ -64,51 +67,66 @@ export const CP_PROJECTS = [
   },
   {
     id: 6,
-    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Personal Portfolio Webpage" src="https://codepen.io/randomsveta/embed/preview/odyppL?height=265&theme-id=0&default-tab=css,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
-      See the Pen <a href='https://codepen.io/randomsveta/pen/odyppL'>FCC: Personal Portfolio Webpage</a> by Sveta
-        (<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
-      </iframe>,
-    name: 'FCC: Personal Portfolio Webpage',
-    taskLink: 'https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-personal-portfolio-webpage',
-    projectLink: 'https://codepen.io/randomsveta/full/odyppL',
-    year: 2018,
+    jsx: <iframe height="400" width='100%' scrolling="no" title="Avocado" src='https://codepen.io/randomsveta/embed/preview/drVOzX?height=265&theme-id=0&default-tab=css,result'
+      frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+      See the Pen <a href='https://codepen.io/randomsveta/pen/drVOzX'>Avocado</a> by Sveta
+(<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
+</iframe>,
+    name: 'Avocado',
+    taskLink: '',
+    projectLink: 'https://codepen.io/randomsveta/full/drVOzX',
+    year: 2019,
     section: 'CodePen'
   },
 
   {
     id: 7,
-    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Technical Documentation Page" src="https://codepen.io/randomsveta/embed/preview/QYQjBg?height=265&theme-id=0&default-tab=html,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
-      See the Pen <a href='https://codepen.io/randomsveta/pen/QYQjBg'>FCC: Technical Documentation Page</a> by Sveta
+    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Random Quote Machine" src="https://codepen.io/randomsveta/embed/preview/oRRVVM?height=265&theme-id=0&default-tab=js,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+      See the Pen <a href='https://codepen.io/randomsveta/pen/oRRVVM'>FCC: Random Quote Machine</a> by Sveta
         (<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
       </iframe>,
-    name: 'FCC: Technical Documentation Pages',
-    taskLink: 'https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-technical-documentation-page',
-    projectLink: 'https://codepen.io/randomsveta/full/QYQjBg',
+    name: 'FCC: Random Quote Machine',
+    taskLink: 'https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-random-quote-machine',
+    projectLink: 'https://codepen.io/randomsveta/full/oRRVVM',
     year: 2019,
     section: 'CodePen'
   },
   {
     id: 8,
-    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Adele-Tribute page" src="https://codepen.io/randomsveta/embed/preview/zjKRJb?height=265&theme-id=0&default-tab=html,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
-      See the Pen <a href='https://codepen.io/randomsveta/pen/zjKRJb'>FCC: Adele-Tribute page</a> by Sveta
+    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Markdown Previewer" src="https://codepen.io/randomsveta/embed/preview/xoXQgr?height=265&theme-id=0&default-tab=js,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+      See the Pen <a href='https://codepen.io/randomsveta/pen/xoXQgr'>FCC: Markdown Previewer</a> by Sveta
         (<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
       </iframe>,
-    name: 'FCC: Adele-Tribute page',
-    taskLink: 'https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-tribute-page',
-    projectLink: 'https://codepen.io/randomsveta/full/zjKRJb',
-    year: 2018,
+    name: 'FCC: Markdown Previewer',
+    taskLink: 'https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer',
+    projectLink: 'https://codepen.io/randomsveta/full/xoXQgr',
+    year: 2019,
     section: 'CodePen'
+
   },
   {
     id: 9,
-    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Product Landing Page" src="https://codepen.io/randomsveta/embed/preview/JZNWVZ?height=265&theme-id=default&default-tab=html,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
-      See the Pen <a href='https://codepen.io/randomsveta/pen/JZNWVZ'>FCC: Product Landing Page</a> by Sveta
+    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Drum Machine" src="https://codepen.io/randomsveta/embed/preview/PrLXLg?height=265&theme-id=0&default-tab=js,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+      See the Pen <a href='https://codepen.io/randomsveta/pen/PrLXLg'>FCC: Drum Machine</a> by Sveta
+      (<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
+    </iframe>,
+    name: 'FCC: Drum Machine',
+    taskLink: 'https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-drum-machine',
+    projectLink: 'https://codepen.io/randomsveta/full/PrLXLg',
+    year: 2019,
+    section: 'CodePen'
+  },
+  {
+    id: 10,
+    jsx: <iframe height="400" width="100%" scrolling="no" title="FCC: Calculator (Immediate Execution Logic)" src="https://codepen.io/randomsveta/embed/preview/VwwPjLK?height=265&theme-id=default&default-tab=js,result" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+      See the Pen <a href='https://codepen.io/randomsveta/pen/VwwPjLK'>FCC: Calculator  (Immediate Execution Logic)</a> by Sveta
         (<a href='https://codepen.io/randomsveta'>@randomsveta</a>) on <a href='https://codepen.io'>CodePen</a>.
       </iframe>,
-    name: 'FCC: Product Landing Page',
-    taskLink: 'https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-product-landing-page',
-    projectLink: 'https://codepen.io/randomsveta/full/JZNWVZ',
+    name: 'FCC: Calculator (Immediate Execution Logic)',
+    taskLink: 'https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-javascript-calculator',
+    projectLink: 'https://codepen.io/randomsveta/full/VwwPjLK',
     year: 2019,
     section: 'CodePen'
   }
+
 ];
