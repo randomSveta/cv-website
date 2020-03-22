75764331c2be347ea61e8731da18cca56a432a5c
Sveta K <test@test.test>
2020-03-22 23:01:26 +0000

    Start work with pagination.

--- src/components/pages/portfolio/sections/codepen-projects/CodePenProjectsDisplay.js
+++ src/components/pages/portfolio/sections/codepen-projects/CodePenProjectsDisplay.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { CP_PROJECTS } from './cp-projects-list';
 import { Row, Col, Container } from 'reactstrap';
 import CodePenProject from './CodePenProject';
+//import Pagination from 'react-router-pagination';
+import PaginationComponent from './PaginationComponent';
+
 
 export default function CodePenProjectsDisplay(props) {
 
@@ -18,6 +21,11 @@ export default function CodePenProjectsDisplay(props) {
             <Row className="m-md-5 px-md-5 py-0 m-2 px-2  justify-content-center align-items-center flex-wrap">
                 {penProjects}
             </Row >
+            <Row>
+                <Col xs={12}>
+                    <PaginationComponent />
+                </Col>
+            </Row>
         </Container>
 
     )
