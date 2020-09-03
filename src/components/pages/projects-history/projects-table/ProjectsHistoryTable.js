132b08cf8dae0300f13409156de9b6ea7cc16277
Sveta K <test@test.test>
2020-09-03 17:58:08 +0100

    Change finished project links logic

--- src/components/pages/projects-history/projects-table/ProjectsHistoryTable.js
+++ src/components/pages/projects-history/projects-table/ProjectsHistoryTable.js
@@ -12,11 +12,29 @@ export default function ProjectsHistory(props) {
     const rows = FINISHED_PROJECTS.filter(project => project.isFinished).sort((a, b) => new Date(b.endDate) - new Date(a.endDate)).map(project => {
         i++;
 
+        const projectLinks = project.projectLinks;
+        const projectLinksList =
+            Array.isArray(projectLinks) && projectLinks.length
+                ?
+                <React.Fragment>
+                    <p className="m-0 p-0"><strong>Solution:</strong></p>
+                    <ul className="list-unstyled m-0 p-0">
+                        {projectLinks.map(linkSet => {
+                            return <li>{<a href={linkSet[1]} target="_blank" rel="noopener noreferrer">{linkSet[0]}</a>}</li>
+                        })}
+                    </ul>
+                </React.Fragment>
+                : null;
+
+
         return (
             <tr key={project.id}>
                 <th scope="row">{i}</th>
                 <td><a href={project.taskLink} target="_blank" rel="noopener noreferrer">{project.title}</a></td>
-                <td>{project.description}<br />{project.projectLink ? <a href={project.projectLink} target="_blank" rel="noopener noreferrer">view project</a> : null}</td>
+                <td>
+                    <p className="m-0 p-0">{project.description}</p><br />
+                    {projectLinksList}
+                </td>
                 <td>{createDate(project.endDate)}</td>
             </tr >);
     })
